608134f81fb72404953a9b3af218aff20c67f28e
artem-00 <artem.malchenko.04@mail.ru>
2024-04-11 05:15:29 +0300

    LocationController 2 lab

--- src/main/java/com/example/gps/service/LocationService.java
+++ src/main/java/com/example/gps/service/LocationService.java
@@ -1,12 +1,10 @@
 package com.example.gps.service;
 
 import com.example.gps.entity.LocationEntity;
-import com.example.gps.entity.UserEntity;
 import com.example.gps.exception.LocationNotFoundException;
 import com.example.gps.model.Location;
 import com.example.gps.model.LocationInfo;
 import com.example.gps.repository.LocationRepository;
-import com.example.gps.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,20 +17,8 @@ public class LocationService {
     @Autowired
     private LocationRepository locationRepo;
 
-    @Autowired
-    private UserRepository userRepo;
-
     public LocationEntity saveLocation(LocationEntity locationEntity) {
         return locationRepo.save(locationEntity);
-    }
-
-    public Location foundLocation(LocationEntity location, Long id) {
-        UserEntity user = userRepo.findById(id).orElse(null);
-        if (user != null) {
-            location.setUser(user);
-            return Location.toModel(locationRepo.save(location));
-        }
-        return null;
     }
 
     public List<LocationInfo> getUserLocations(Long userId) {
@@ -48,7 +34,7 @@ public class LocationService {
     }
 
     public Location getLocation(Long id) throws LocationNotFoundException {
-        LocationEntity location = locationRepo.findById(id).get();
+        LocationEntity location = locationRepo.findById(id).orElse(null);
         if (location == null){
             throw new LocationNotFoundException("Location not found");
         }
@@ -70,7 +56,4 @@ public class LocationService {
 
         locationRepo.save(location);
     }
-
-
-
 }
